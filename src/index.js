b07050b214140eca21605066aae3f630d34fef59
MadKarma <100418457+madkarmaa@users.noreply.github.com>
2024-01-25 12:27:22 +0100

    feat: moved files to specific folders

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const axios = require('axios').default;
 const { URL } = require('node:url');
 const { getVideoDurationInSeconds } = require('get-video-duration');
 const convert = require('convert-units');
+const endpoints = require('./constants/endpoints.js');
 
 axios.interceptors.response.use(
     function (response) {
@@ -15,26 +16,6 @@ axios.interceptors.response.use(
         });
     }
 );
-
-const BASE_URL = 'https://api-f.streamable.com/api/v1';
-
-const endpoints = {
-    LOGIN: 'https://ajax.streamable.com/check',
-    ME: BASE_URL + '/me',
-    SUBSCRIPTION_INFO: BASE_URL + '/me/subscription/info',
-    LABELS: BASE_URL + '/labels',
-    EXTRACT: function (encodedUrl) {
-        return BASE_URL + '/extract?url=' + encodedUrl;
-    },
-    UPLOAD_FROM_URL: BASE_URL + '/uploads/videos',
-    TRANSCODE_FROM_URL: function (videoId) {
-        return BASE_URL + '/transcode/' + videoId;
-    },
-    INIT_UPLOAD_VIDEO: function (fileSize) {
-        return BASE_URL + '/uploads/shortcode?size=' + fileSize + '&version=unknown';
-    },
-    VIDEOS: BASE_URL + '/videos?sort=date_added',
-};
 
 /**
  * Client to interact with the `streamable.com` API
