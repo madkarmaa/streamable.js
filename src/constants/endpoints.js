c2b5a66015f1c79917b190e2bd99993285d6eba9
MadKarma <100418457+madkarmaa@users.noreply.github.com>
2024-01-28 20:30:06 +0100

    refactor: shortened functions to arrow functions

--- src/constants/endpoints.js
+++ src/constants/endpoints.js
@@ -6,20 +6,12 @@ const endpoints = {
     ME: BASE_URL + '/me',
     SUBSCRIPTION_INFO: BASE_URL + '/me/subscription/info',
     LABELS: BASE_URL + '/labels',
-    EXTRACT: function (encodedUrl) {
-        return BASE_URL + '/extract?url=' + encodedUrl;
-    },
+    EXTRACT: (encodedUrl) => BASE_URL + '/extract?url=' + encodedUrl,
     UPLOAD_FROM_URL: BASE_URL + '/uploads/videos',
-    TRANSCODE_FROM_URL: function (videoId) {
-        return BASE_URL + '/transcode/' + videoId;
-    },
-    INIT_UPLOAD_VIDEO: function (fileSize) {
-        return BASE_URL + '/uploads/shortcode?size=' + fileSize + '&version=unknown';
-    },
+    TRANSCODE_FROM_URL: (videoId) => BASE_URL + '/transcode/' + videoId,
+    INIT_UPLOAD_VIDEO: (fileSize) => BASE_URL + '/uploads/shortcode?size=' + fileSize + '&version=unknown',
     VIDEOS: BASE_URL + '/videos?sort=date_added',
-    VIDEO: function (shortcode) {
-        return BASE_URL + '/videos/' + shortcode;
-    },
+    VIDEO: (shortcode) => BASE_URL + '/videos/' + shortcode,
 };
 
 module.exports = endpoints;
